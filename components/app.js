207decf2ebbe4904d6fc8d55749f87f798b68a70
Tarcisio Oliveira <t@rcis.io>
2024-09-27 23:54:04 -0300

    update

--- components/app.js
+++ components/app.js
@@ -3,12 +3,29 @@
 import '@patternfly/react-core/dist/styles/base.css';
 
 import {
+	Masthead,
+	MastheadContent,
+	MastheadMain,
 	Page,
 	PageSection,
 	PageSidebar
 } from '@patternfly/react-core';
 
 export function App({ children }) {
+
+	const masthead = (
+		<>
+
+			<Masthead>
+
+				<MastheadMain></MastheadMain>
+
+				<MastheadContent></MastheadContent>
+
+			</Masthead>
+
+		</>
+	);
 
 	const sidebar = (
 		<>
@@ -21,7 +38,8 @@ export function App({ children }) {
 	return (
 		<>
 
-			<Page sidebar         = { sidebar }
+			<Page masthead        = { masthead }
+				  sidebar         = { sidebar }
 				  isContentFilled = { true }>
 
 				<PageSection isFilled = { true }>{ children }</PageSection>
