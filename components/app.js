5ff3ef3094277f450baf7d4a9534918ddbdd8dc7
Tarcisio Oliveira <t@rcis.io>
2024-09-27 23:45:06 -0300

    update

--- components/app.js
+++ components/app.js
@@ -4,15 +4,25 @@ import '@patternfly/react-core/dist/styles/base.css';
 
 import {
 	Page,
-	PageSection
+	PageSection,
+	PageSidebar
 } from '@patternfly/react-core';
 
 export function App({ children }) {
 
+	const sidebar = (
+		<>
+
+			<PageSidebar isSidebarOpen = { false }></PageSidebar>
+
+		</>
+	);
+
 	return (
 		<>
 
-			<Page isContentFilled = { true }>
+			<Page sidebar         = { sidebar }
+				  isContentFilled = { true }>
 
 				<PageSection isFilled = { true }>{ children }</PageSection>
 
