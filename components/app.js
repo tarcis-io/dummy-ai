b89ef39e8d533eec42c5c8f8b94761245c636c73
Tarcisio Oliveira <t@rcis.io>
2024-09-27 23:24:25 -0300

    update

--- components/app.js
+++ components/app.js
@@ -12,9 +12,9 @@ export function App({ children }) {
 	return (
 		<>
 
-			<Page>
+			<Page isContentFilled = { true }>
 
-				<PageSection>{ children }</PageSection>
+				<PageSection isFilled = { true }>{ children }</PageSection>
 
 			</Page>
 
