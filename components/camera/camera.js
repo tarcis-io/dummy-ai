a528947170c15e15f27caad2eaa0a3736784d64f
Tarcisio Oliveira <t@rcis.io>
2024-09-29 15:33:23 -0300

    update

--- components/camera/camera.js
+++ components/camera/camera.js
@@ -2,6 +2,17 @@
 
 export default function Camera() {
 
+	const mediaStreamConstraints = {
+		audio : true,
+		video : true
+	};
+
+	navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(mediaStream => {
+
+		const video = document.getElementById('video');
+		video.srcObject = mediaStream;
+	});
+
 	return (
 		<>
 
