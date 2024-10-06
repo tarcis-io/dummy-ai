d31acd0639a7f3f0a794b3b6b45645e5e772894f
Tarcisio Oliveira <t@rcis.io>
2024-10-06 03:37:46 -0300

    update

--- components/camera/camera.js
+++ components/camera/camera.js
@@ -1,31 +1,9 @@
-'use client';
-
-import {
-	useState
-} from 'react';
-
-import {
-	Card
-} from '@patternfly/react-core';
-
-import CameraIsLoading from './camera-is-loading';
-
 export default function Camera() {
-
-	const Status = {
-		LOADING : 'loading'
-	};
-
-	const [status, setStatus] = useState(Status.LOADING);
 
 	return (
 		<>
 
-			<Card>
-
-				<CameraIsLoading />
-
-			</Card>
+			<div>Camera</div>
 
 		</>
 	);
