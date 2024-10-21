ae9f84e2e968d41e751ca778e30fc06c38fd916f
Tarcisio Oliveira <t@rcis.io>
2024-10-21 15:39:12 -0300

    update

--- components/camera/camera.js
+++ components/camera/camera.js
@@ -1,13 +1,9 @@
-import CameraError   from './camera-error';
-import CameraLoading from './camera-loading';
-
 export default function Camera() {
 
 	return (
 		<>
 
-			<CameraError title = { 'Error...' }
-						 text  = { 'Camera Error!' } />
+			<div>Camera</div>
 
 		</>
 	);
