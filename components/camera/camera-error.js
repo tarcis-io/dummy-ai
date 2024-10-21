444e66fc70f45aeb491cf56a75988e16ddfffa1e
Tarcisio Oliveira <t@rcis.io>
2024-10-21 00:11:35 -0300

    update

--- components/camera/camera-error.js
+++ components/camera/camera-error.js
@@ -1,16 +1,24 @@
 'use client';
 
 import {
-	Alert
+	Alert,
+	Button
 } from '@patternfly/react-core';
 
 export default function CameraError({ title, text }) {
 
+	const reloadThisPage = function () {
+		window.location.reload();
+	};
+
 	return (
 		<>
 
-			<Alert variant = { 'danger' }
-				   title   = { title }>
+			<Alert variant     = { 'danger' }
+				   title       = { title }
+				   actionLinks = { <Button variant  = { 'secondary' }
+										   isDanger = { true }
+										   onClick  = { reloadThisPage }>Reload this page</Button> }>
 
 				<p>{ text }</p>
 
