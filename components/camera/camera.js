a0a380848797caab8bb201348e09c1778a17629e
Tarcisio Oliveira <t@rcis.io>
2024-10-21 15:50:02 -0300

    update

--- components/camera/camera.js
+++ components/camera/camera.js
@@ -1,4 +1,14 @@
+import {
+	useState
+} from 'react';
+
 export default function Camera() {
+
+	const Status = {
+		LOADING : 'loading'
+	};
+
+	const [status, setStatus] = useState(Status.LOADING);
 
 	return (
 		<>
